844d27311f9b1fb3d4f1c866dd0121fb446968b1
Uzair Shamim <uzashamim@gmail.com>
2024-01-22 22:03:55 -0500

    Initial commit

---
+++ SimpleTwitch/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Web.WebView2.Core;
+
+namespace SimpleTwitch
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SimpleTwitch/Services/EventService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTwitch.Services {
+    internal class EventService {
+        public event Action<string> OnEnterPressed;
+        
+        public void HandleChangeUser(
+            string user
+        ) {
+            OnEnterPressed?.Invoke(user);
+        }
+    }
+}
